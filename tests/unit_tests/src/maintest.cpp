3e2b961a11d2dfc98fb752c24f7cd5ca3ea9b502
Andrii <kovalchukworks@gmail.com>
2019-12-02 19:29:53 +0200

    init

---
+++ tests/unit_tests/src/maintest.cpp
@@ -0,0 +1,14 @@
+#include "maintest.h"
+#include "impl.h"
+
+TEST(Lab1,Simple)
+{
+   int source = 0x5;
+   char* expected = "00000000000000000000000000000101";
+   char buffer[80];
+
+   char *actual = IntToBinaryRepresentation(buffer,source);
+
+
+   ASSERT_STREQ(expected,actual);
+}
