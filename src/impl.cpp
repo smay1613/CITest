835173187e7d333a806cae9418ccb1db9c156ed7
Oleh Komarnytskyi <okomarnytskyi@luxoft.com>
2019-12-03 16:31:31 +0200

    Hometask 2 [card-30015274]

--- src/impl.cpp
+++ src/impl.cpp
@@ -1,1 +1,16 @@
 #include "impl.h"
+
+int BinaryInvert(int x,int p,int n)
+{
+    constexpr int INT32_BIT_COUNT = sizeof(x)*8;
+
+    if (p < INT32_BIT_COUNT && n <= p)
+    {
+        for (int i = p; i > p - n; i--)
+        {
+            x ^= 1 << (i-1);
+        }
+    }
+
+    return x;
+}
