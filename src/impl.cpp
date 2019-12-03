1a8df5ed4949c67c3b1b21f340079a749261b0b0
GOST-UA <10502791+GOST-UA@users.noreply.github.com>
2019-12-03 15:24:30 +0200

    Add IntToBinaryRepresentation implementation

--- src/impl.cpp
+++ src/impl.cpp
@@ -1,1 +1,10 @@
 #include "impl.h"
+
+char* IntToBinaryRepresentation(char *buf, int num) {
+  for (int i = sizeof(int) * 8, j = 0; i > 0; i--, j++) {
+    buf[i - 1] = ((num >> j) & 0x01) ? '1' : '0';
+  }
+
+  buf[sizeof(int) * 8] = '\0';
+  return buf;
+}
