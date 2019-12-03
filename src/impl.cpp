8ce50457594154b1da02f31215c4c08a20b194ae
onixyara <onixyarka@gmail.com>
2019-12-03 12:41:14 +0200

    Implements homework

--- src/impl.cpp
+++ src/impl.cpp
@@ -5,8 +5,8 @@
 char* IntToBinaryRepresentation(char *buffer,int number)
 {
 	std::bitset<32> y(number);
-	std::string str = y.to_string();
-	strcpy(buffer, str.c_str());
+    std::string string = y.to_string();
+    strcpy(buffer, string.c_str());
 
 	return (buffer);
 }
