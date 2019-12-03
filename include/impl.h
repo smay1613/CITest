c5cbce8f6dfd8fd5dcf25e0063739a44dd8e9a12
PaulSergienko <paulsergienko@gmail.com>
2019-12-03 19:09:44 +0200

    Implements homework

--- include/impl.h
+++ include/impl.h
@@ -1,1 +1,2 @@
 #pragma once
+bool Quadro(int a,int b,int c, double& x1,double& x2);
--- src/impl.cpp
+++ src/impl.cpp
@@ -1,1 +1,7 @@
 #include "impl.h"
+#include <math.h>
+
+bool Quadro(int a,int b,int c, double& x1,double& x2)
+{
+    return (b*b - 4*a*c) >= 0 ? x1 = ((-b + sqrt(b*b - 4*a*c))/2/a), x2 = ((-b - sqrt(b*b - 4*a*c))/2/a), true : false;
+}
