cfff2920c5ae7978d07d6ffe4040308829839d7d
Vladislav Onishchuk <vladislav.onishchuk-EXT@continental-corporation.com>
2019-12-03 11:30:05 +0200

    .

--- src/impl.cpp
+++ src/impl.cpp
@@ -1,1 +1,12 @@
 #include "impl.h"
+#include <bitset>
+#include <cstring>
+
+char* IntToBinaryRepresentation(char *buffer,int number)
+{
+	std::bitset<32> y(number);
+	std::string str = y.to_string();
+	strcpy(buffer, str.c_str());
+
+	return (buffer);
+}
