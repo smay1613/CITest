fb01cfc69876de8b3105a99b9860ee38f37e0a9f
Vladislav Onishchuk <vladislav.onishchuk-EXT@continental-corporation.com>
2019-12-04 13:39:39 +0200

    .

--- src/impl.cpp
+++ src/impl.cpp
@@ -1,5 +1,9 @@
 #include "impl.h"
-#include <bitset>
-#include <cstring>
-#include <limits.h>
+#include <cmath>
 
+bool Quadro(int a,int b,int c, double& x1,double& x2)
+{
+	return (x1 = (-b + (sqrt(static_cast<double>(b*b - 4*a*c))))/(2*a),
+			x2 = (-b - (sqrt(static_cast<double>(b*b - 4*a*c))))/(2*a),
+			!std::isnan(sqrt(static_cast<double>(b*b - 4*a*c))));
+}
