a769d65e1ec4f1d6f41ecb65e1af089537ad1c89
Vladislav Onishchuk <vladislav.onishchuk-EXT@continental-corporation.com>
2019-12-06 12:36:23 +0200

    Task5 implementation

--- include/impl.h
+++ include/impl.h
@@ -1,1 +1,18 @@
 #pragma once
+
+class Complex {
+private:
+	int _re;
+	int _im;
+public:
+	Complex(int re = 0,int im = 0);
+	virtual ~Complex();
+	int getRe() const { return _re;}
+	int getIm() const { return _im;}
+	Complex operator+(const Complex& op);
+
+	Complex operator-(const Complex &op);
+
+};
+
+bool operator==(const Complex &op1,const Complex &op2);
--- src/impl.cpp
+++ src/impl.cpp
@@ -1,1 +1,32 @@
 #include "impl.h"
+
+Complex::Complex(int re,int im): _re(re),_im(im) {
+}
+
+Complex::~Complex() {
+}
+
+Complex Complex::operator+(const Complex& op)
+{
+	_re += op.getRe();
+	_im += op.getIm();
+
+	return Complex();
+}
+
+Complex Complex::operator-(const Complex &op)
+{
+	_re -= op.getRe();
+	_im -= op.getIm();
+
+	return Complex();
+}
+
+bool operator==(const Complex &op1,const Complex &op2)
+{
+	if ((op1.getRe() == op2.getRe()) && (op1.getIm() == op2.getIm()))
+	{
+		return true;
+	}
+	return false;
+}
