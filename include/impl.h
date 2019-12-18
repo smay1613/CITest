bfb92575d0591ef6e61d5f7a06d366f3f19955df
SavinKG <savinkg@gmail.com>
2019-12-18 19:07:53 +0200

    Hometask lab9

--- include/impl.h
+++ include/impl.h
@@ -1,1 +1,13 @@
 #pragma once
+#include <vector>
+
+class Integer
+{
+private:
+    int _value {0};
+    char *_buffer{nullptr};
+public:
+    Integer(int value);
+    ~Integer();
+    operator char*();
+};
--- src/impl.cpp
+++ src/impl.cpp
@@ -1,1 +1,33 @@
 #include "impl.h"
+#include <exception>
+#include <stdexcept>
+
+Integer::Integer(int value):_value(value)
+{
+    if ((value>=0) && (value<10))
+    {    std::vector<char*> literalNumbers {"zero",
+                                       "one",
+                                       "two",
+                                       "three",
+                                       "four",
+                                       "five",
+                                       "six",
+                                       "seven",
+                                       "eight",
+                                       "nine"};
+        _buffer = const_cast<char*> (literalNumbers[value]);
+    }
+    else
+    {
+        throw std::out_of_range ("wrong value");
+    }
+}
+
+Integer::~Integer()
+{
+}
+
+Integer::operator char *()
+{
+    return _buffer;
+}
